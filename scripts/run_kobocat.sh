ccd004edee33b01d631ba0bc6cf722acea73f6d3
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2019-11-28 17:19:47 +0545

    celery upstart added

--- scripts/run_kobocat.sh
+++ scripts/run_kobocat.sh
@@ -3,3 +3,6 @@ python manage.py fieldsight_default_commands
 python manage.py create_default_superuser
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8001
+# start the celery 
+export C_FORCE_ROOT='true'
+celery -A onadata worker -l Info
