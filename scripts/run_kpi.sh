44c886e8b12eab93081ea28ae95692beb6ebc710
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2019-12-02 15:30:44 +0545

    update in scrips

--- scripts/run_kpi.sh
+++ scripts/run_kpi.sh
@@ -1,7 +1,6 @@
-python manage.py migrate --noinput
 npm install
 npm run build
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
-
 
--- scripts/wait_for_mongo.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo 'Waiting for container `mongo`.'
-dockerize -timeout=40s -wait tcp://${KOBO_MONGO_HOST}:${KOBO_MONGO_PORT}
-echo 'Container `mongo` up.'
