a622628d2dffe546869308e304587be88506e740
Amulya Acharya <awemulya@gmail.com>
2020-01-27 17:14:25 +0545

    clean up docker configs

--- scripts/run_fieldsight.sh
+++ scripts/run_fieldsight.sh
@@ -3,8 +3,5 @@
 #python manage.py create_default_superuser
 python manage.py migrate --noinput
 python manage.py delete_permission
-python manage.py collectstatic --noinput
-export C_FORCE_ROOT="true"
-screen -dmS screen_celery_fieldsight bash -c 'celery -A onadata worker -l Info; exec bash'
 python manage.py runserver 0.0.0.0:8001
 
--- scripts/run_kpi.sh
+++ scripts/run_kpi.sh
@@ -1,8 +1,3 @@
-npm install -g
-npm run build
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-export C_FORCE_ROOT="true"
-screen -dmS screen_celery_kpi bash -c 'celery -A kobo worker -l Info; exec bash'
 python manage.py runserver 0.0.0.0:8000
 
